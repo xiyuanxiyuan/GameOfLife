a4de4b35db1965a4771410cdac941ee37fce99d7
xiyuanxiyuan <646221237@qq.com>
2020-07-23 17:09:08 +0800

    Game Of Life(simple version)

---
+++ src/main/java/GameInterface.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class GameInterface {
+
+    JFrame jFrame;
+    JPanel jPanel;
+    JLabel[][] jLabels;
+
+    GameInterface(int height,int width){
+        jFrame=new JFrame();//主框架
+        jPanel=new JPanel();
+        jLabels=new JLabel[height][width];
+        ImageIcon imageIcon=new ImageIcon("like.png");
+
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++){//初始化每个小格子的内容
+                jLabels[i][j]=new JLabel();
+                jLabels[i][j].setOpaque(true);
+
+                jLabels[i][j].setHorizontalAlignment(SwingConstants.CENTER);//标签内容居中
+                jLabels[i][j].setVerticalAlignment(SwingConstants.CENTER);
+
+                jLabels[i][j].setBorder(BorderFactory.createLineBorder(Color.black));//标签边框
+
+                jPanel.add(jLabels[i][j]);//将label添加到panel中
+            }
+            jPanel.setLayout(new GridLayout(height,width,0,0));
+        }
+
+        jFrame.add(jPanel);
+        jFrame.setSize(1000,1000);
+        jFrame.setResizable(false);
+        jFrame.setUndecorated(false);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void go(){
+
+    }
+
+    public void refresh(int setOfCells[][],int height,int width){
+        for(int i=0;i<height;i++) {
+            for (int j = 0; j < width; j++) {
+                if(setOfCells[i][j]==1){
+                    jLabels[i][j].setBackground(Color.black);
+                }else {
+                    jLabels[i][j].setBackground(Color.white);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/GameOfLifeMain.java
@@ -0,0 +1,286 @@
+import java.awt.*;
+import java.sql.Time;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameOfLifeMain {
+
+    int[][] setOfCells;
+    int height=2, width=2,maxDensity=6,midDensity=4,minDensity=2;
+    GameInterface gameInterface;
+
+    public static void main(String[] args){
+
+        GameOfLifeMain gameOfLifeMain=new GameOfLifeMain();
+        gameOfLifeMain.init();
+        while (true){
+            gameOfLifeMain.run();
+        }
+    }
+
+    public void init(){//初始化
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("高？");
+        height=scanner.nextInt();
+        System.out.println("宽？");
+        width=scanner.nextInt();
+        System.out.println("最大密度？");
+        maxDensity=scanner.nextInt();
+        System.out.println("稳定点？");
+        midDensity=scanner.nextInt();
+        System.out.println("最小密度？");
+        minDensity=scanner.nextInt();
+        gameInterface=new GameInterface(height,width);
+        Random random=new Random();//随机生成世界
+
+        setOfCells=new int[height][width];//初始化细胞群
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++) {
+                //setOfCells[i][j] = random.nextBoolean()?1:0;
+                setOfCells[i][j] = random.nextFloat()>0.5?1:0;
+            }
+        }
+        gameInterface.refresh(setOfCells,height,width);
+
+
+
+    }
+
+    public void run(){
+
+        int[][] copyOfSetOfCells=new int[height][width];
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++) {
+                copyOfSetOfCells[i][j]=setOfCells[i][j];
+            }
+        }
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++) {
+                int count=0;
+                if(i==0&&j==0){//左上角
+                    if(setOfCells[i][j+1]==1){
+                        count++;
+                    }
+                    if(setOfCells[i+1][j]==1){
+                        count++;
+                    }
+                    if(setOfCells[i+1][j+1]==1){
+                        count++;
+                    }
+                    if(count>=maxDensity||count<=minDensity){
+                        copyOfSetOfCells[i][j]=0;
+                    }else if(count<maxDensity&&count>=midDensity){
+                        copyOfSetOfCells[i][j]=1;
+                    }else if(count<midDensity&&count>minDensity){
+                        //状态不变
+                    }
+                    continue;
+                }
+                if(i==0&&j!=0&&j!=width-1){//上边
+                    if(setOfCells[i][j-1]==1){//左
+                        count++;
+                    }
+                    if(setOfCells[i][j+1]==1){//右
+                        count++;
+                    }
+                    if(setOfCells[i+1][j-1]==1){//左下
+                        count++;
+                    }
+                    if(setOfCells[i+1][j]==1){//下
+                        count++;
+                    }
+                    if(setOfCells[i+1][j+1]==1){//右下
+                        count++;
+                    }
+                    if(count>=maxDensity||count<=minDensity){
+                        copyOfSetOfCells[i][j]=0;
+                    }else if(count<maxDensity&&count>=midDensity){
+                        copyOfSetOfCells[i][j]=1;
+                    }else if(count<midDensity&&count>minDensity){
+                        //状态不变
+                    }
+                    continue;
+                }
+                if(i==0&&j==width-1){//右上角
+                    if(setOfCells[i][j-1]==1){//左
+                        count++;
+                    }
+                    if(setOfCells[i+1][j-1]==1){//左下
+                        count++;
+                    }
+                    if(setOfCells[i+1][j]==1){//下
+                        count++;
+                    }
+                    if(count>=maxDensity||count<=minDensity){
+                        copyOfSetOfCells[i][j]=0;
+                    }else if(count<maxDensity&&count>=midDensity){
+                        copyOfSetOfCells[i][j]=1;
+                    }else if(count<midDensity&&count>minDensity){
+                        //状态不变
+                    }
+                    continue;
+                }
+                if(j==0&&i!=0&&i!=height-1){//左边
+                    if(setOfCells[i-1][j]==1){//上
+                        count++;
+                    }
+                    if(setOfCells[i-1][j+1]==1){//右上
+                        count++;
+                    }
+                    if(setOfCells[i][j+1]==1){//右
+                        count++;
+                    }
+                    if(setOfCells[i+1][j]==1){//下
+                        count++;
+                    }
+                    if(setOfCells[i+1][j+1]==1){//右下
+                        count++;
+                    }
+                    if(count>=maxDensity||count<=minDensity){
+                        copyOfSetOfCells[i][j]=0;
+                    }else if(count<maxDensity&&count>=midDensity){
+                        copyOfSetOfCells[i][j]=1;
+                    }else if(count<midDensity&&count>minDensity){
+                        //状态不变
+                    }
+                    continue;
+                }
+                if(j==width-1&&i!=0&&i!=height-1){//右边
+                    if(setOfCells[i-1][j-1]==1){//左上
+                        count++;
+                    }
+                    if(setOfCells[i-1][j]==1){//上
+                        count++;
+                    }
+                    if(setOfCells[i][j-1]==1){//左
+                        count++;
+                    }
+                    if(setOfCells[i+1][j-1]==1){//左下
+                        count++;
+                    }
+                    if(setOfCells[i+1][j]==1){//下
+                        count++;
+                    }
+                    if(count>=maxDensity||count<=minDensity){
+                        copyOfSetOfCells[i][j]=0;
+                    }else if(count<maxDensity&&count>=midDensity){
+                        copyOfSetOfCells[i][j]=1;
+                    }else if(count<midDensity&&count>minDensity){
+                        //状态不变
+                    }
+                    continue;
+                }
+                if(i==height-1&&j==0){//左下角
+                    if(setOfCells[i-1][j]==1){//上
+                        count++;
+                    }
+                    if(setOfCells[i-1][j+1]==1){//右上
+                        count++;
+                    }
+                    if(setOfCells[i][j+1]==1){//右
+                        count++;
+                    }
+                    if(count>=maxDensity||count<=minDensity){
+                        copyOfSetOfCells[i][j]=0;
+                    }else if(count<maxDensity&&count>=midDensity){
+                        copyOfSetOfCells[i][j]=1;
+                    }else if(count<midDensity&&count>minDensity){
+                        //状态不变
+                    }
+                    continue;
+                }
+                if(i==height-1&&j!=0&&j!=width-1){//下边
+                    if(setOfCells[i-1][j-1]==1){//左上
+                        count++;
+                    }
+                    if(setOfCells[i-1][j]==1){//上
+                        count++;
+                    }
+                    if(setOfCells[i-1][j+1]==1){//右上
+                        count++;
+                    }
+                    if(setOfCells[i][j-1]==1){//左
+                        count++;
+                    }
+                    if(setOfCells[i][j+1]==1){//右
+                        count++;
+                    }
+                    if(count>=maxDensity||count<=minDensity){
+                        copyOfSetOfCells[i][j]=0;
+                    }else if(count<maxDensity&&count>=midDensity){
+                        copyOfSetOfCells[i][j]=1;
+                    }else if(count<midDensity&&count>minDensity){
+                        //状态不变
+                    }
+                    continue;
+                }
+                if(i==height-1&&j==width-1){//右下角
+                    if(setOfCells[i-1][j-1]==1){//左上
+                        count++;
+                    }
+                    if(setOfCells[i-1][j]==1){//上
+                        count++;
+                    }
+                    if(setOfCells[i][j-1]==1){//左
+                        count++;
+                    }
+                    if(count>=maxDensity||count<=minDensity){
+                        copyOfSetOfCells[i][j]=0;
+                    }else if(count<maxDensity&&count>=midDensity){
+                        copyOfSetOfCells[i][j]=1;
+                    }else if(count<midDensity&&count>minDensity){
+                        //状态不变
+                    }
+                    continue;
+                }
+
+                if(setOfCells[i-1][j-1]==1){//左上
+                    count++;
+                }
+                if(setOfCells[i-1][j]==1){//上
+                    count++;
+                }
+                if(setOfCells[i-1][j+1]==1){//右上
+                    count++;
+                }
+                if(setOfCells[i][j-1]==1){//左
+                    count++;
+                }
+                if(setOfCells[i][j+1]==1){//右
+                    count++;
+                }
+                if(setOfCells[i+1][j-1]==1){//左下
+                    count++;
+                }
+                if(setOfCells[i+1][j]==1){//下
+                    count++;
+                }
+                if(setOfCells[i+1][j+1]==1){//右下
+                    count++;
+                }
+                if(count>=maxDensity||count<=minDensity){
+                    copyOfSetOfCells[i][j]=0;
+                }else if(count<maxDensity&&count>=midDensity){
+                    copyOfSetOfCells[i][j]=1;
+                }else if(count<midDensity&&count>minDensity){
+                    //状态不变
+                }
+
+            }
+        }
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++) {
+                setOfCells[i][j]=copyOfSetOfCells[i][j];
+            }
+        }
+        gameInterface.refresh(setOfCells,height,width);
+        Robot robot= null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+        robot.delay(500);
+
+    }
+}
